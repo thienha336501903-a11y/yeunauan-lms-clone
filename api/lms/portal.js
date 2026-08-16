f10f77634e967b17fdca6f4f1af1ab9cab503261
thienha336501903-a11y <thienha336501903@gmail.com>
2026-08-16 17:39:13 +0700

    feat: route protected V4 playback leases

--- api/lms/portal.js
+++ api/lms/portal.js
@@ -7,6 +7,7 @@ import learningModeHandler from "../../utils/lms-handlers/learning-mode.js";
 import v3BootstrapHandler from "../../utils/lms-handlers/v3-bootstrap.js";
 import v4TelegramFeedHandler from "../../utils/lms-handlers/v4-telegram-feed.js";
 import v4TelegramMediaHandler from "../../utils/lms-handlers/v4-telegram-media.js";
+import v4TelegramPlayHandler from "../../utils/lms-handlers/v4-telegram-play.js";
 import v4TelegramThumbnailHandler from "../../utils/lms-handlers/v4-telegram-thumbnail.js";
 import v4TelegramWarmupHandler from "../../utils/lms-handlers/v4-telegram-warmup.js";
 
@@ -39,6 +40,9 @@ export default async function handler(req, res) {
   }
   if (endpoint === "v4-telegram-media") {
     return v4TelegramMediaHandler(req, res);
+  }
+  if (endpoint === "v4-telegram-play") {
+    return v4TelegramPlayHandler(req, res);
   }
   if (endpoint === "v4-telegram-thumbnail") {
     return v4TelegramThumbnailHandler(req, res);
