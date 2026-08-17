a983fa9ea5d9df06f3721d8a020e002ce4dd0782
thienha336501903-a11y <thienha336501903@gmail.com>
2026-08-17 23:15:18 +0700

    chore: trigger V4-test build after watermark removal

--- v4-media-sw.js
+++ v4-media-sw.js
@@ -162,3 +162,5 @@ self.addEventListener("fetch", event => {
   }
   event.respondWith(proxyMedia(event.request, leaseId));
 });
+
+// Build marker: student email watermark removed; playback protection behavior unchanged.
